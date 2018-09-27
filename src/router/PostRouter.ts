16c79aaac849325953e4a555cb8b23aef3851597
Charpell <charpellumeh@gmail.com>
2018-09-27 05:10:50 +0100

    Create Update and Delete Posts

--- src/router/PostRouter.ts
+++ src/router/PostRouter.ts
@@ -9,6 +9,7 @@ class PostRouter {
     this.routes();
   }
 
+  // get all of the posts in the database
   public GetPosts(req: Request, res: Response): void {
     Post.find()
       .then(data => {
@@ -19,18 +20,84 @@ class PostRouter {
       });
   }
 
-  public GetPost(req: Request, res: Response): void {}
+  // get a single post by params of 'slug'
+  public GetPost(req: Request, res: Response): void {
+    const { slug } = req.params;
 
-  public CreatePost(req: Request, res: Response): void {}
+    Post.findOne({ slug })
+      .then((data) => {
+        res.status(200).json({ data });
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
+  }
 
-  public UpdatePost(req: Request, res: Response): void {}
+  // create a new post
+  public CreatePost(req: Request, res: Response): void {
+    const {
+      title,
+      slug,
+      content,
+      featuredImage,
+      category,
+      published,
+    } = req.body;
 
-  public DeletePost(req: Request, res: Response): void {}
+    const post = new Post({
+      title,
+      slug,
+      content,
+      featuredImage,
+      category,
+      published,
+    });
+
+    post
+      .save()
+      .then((data) => {
+        res.status(201).json({ data });
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
+  }
+
+ // update post by params of 'slug'
+  public UpdatePost(req: Request, res: Response): void {
+    const { slug } = req.body;
+
+    Post.findOneAndUpdate({ slug }, req.body)
+      .then((data) => {
+        res.status(200).json({ data });
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
+  }
+
+
+  // delete post by params of 'slug'
+  public DeletePost(req: Request, res: Response): void {
+    const { slug } = req.body;
+
+    Post.findOneAndRemove({ slug })
+      .then(() => {
+        res.status(204).end();
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
+  }
 
 
 
   routes() {
-    this.router.get('/', this.GetPosts)
+    this.router.get('/', this.GetPosts);
+    this.router.get('/:slug', this.GetPost);
+    this.router.post('/', this.CreatePost);
+    this.router.put('/:slug', this.UpdatePost);
+    this.router.delete('/:slug', this.DeletePost);
   }
 }
 
